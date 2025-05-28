a6ade9db90851f40d1b2d66f64214c212b9845e9
Aboba <eirikrinnrauthr@tutanota.com>
2025-05-28 14:15:10 +0100

    perf: remove prescient flag from helm

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
        :completion
        ;;company           ; the ultimate code completion backend
        (corfu +orderless)  ; complete with cap(f), cape and a flying feather!
-       (helm +fuzzy +prescient +icons)              ; the *other* search engine for love and life
+       (helm +fuzzy +icons)              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
        ;; (vertico +icons)           ; the search engine of the future
