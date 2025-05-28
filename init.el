9ba9748329bf3e8c8e2961e685115b0b89a68c3a
Aboba <eirikrinnrauthr@tutanota.com>
2025-05-28 14:15:51 +0100

    chore: remove jounal in favour of org-roam
    Add drag and drop for images

--- init.el
+++ init.el
@@ -154,7 +154,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam2 +journal +noter +present)               ; organize your plain life in plain text
+       (org +roam2 +noter +present +dragndrop)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
