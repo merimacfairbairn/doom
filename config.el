139cd6e4691e9272985e749f016d6c544b4a2901
Aboba <eirikrinnrauthr@tutanota.com>
2025-05-28 14:12:27 +0100

    feat: add org-roam-ui package

--- config.el
+++ config.el
@@ -148,8 +148,20 @@
                               "#+title: %<%Y-%m-%d>\n"))))
   :config
   (org-roam-db-autosync-mode))
-  :config
 
+;; Org-roam UI
+(use-package! websocket
+  :after org-roam)
+
+(use-package! org-roam-ui
+  :after org-roam
+  :config
+  (setq org-roam-ui-sync-theme t
+        org-roam-ui-follow t
+        org-roam-ui-update-on-save t
+        org-roam-ui-open-on-start t))
+
+(map! :leader :prefix "n r" :desc "Org-roam UI graph" "g" #'org-roam-ui-open)
 
 (use-package org-superstar
   :config
